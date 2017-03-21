020f1d967dda5c365d4f43601cb5890c165489da
Andriy Tkachenko <1akaant1@gmail.com>
2017-03-21 17:55:29 +0000

    Add favicon

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const db = require('./config/db')
 
@@ -13,6 +14,8 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
